ba9677fe92165f4a1d9901b44790516611dd2d17
Dianne <mdianne.gabriel@gmail.com>
2021-10-18 21:09:20 -0400

    Build multiple divs for flexbox

--- src/components/ActivityDetail.jsx
+++ src/components/ActivityDetail.jsx
@@ -17,17 +17,25 @@ const ActivityDetail = () => {
 
   return (
     <div className="call-detail">
-      I am ActivityDetail inside ActivityFeed
-      <p>Number of calls: {activity.length}</p>
-      <p>Call type: {activity[0].call_type}</p>
-      <p>Created at: {activity[0].created_at}</p>
-      <p>Direction: {activity[0].direction}</p>
-      <p>Duration: {activity[0].duration}</p>
-      <p>From: {activity[0].from}</p>
-      <p>ID: {activity[0].id}</p>
-      <p>Archived?: {activity[0].is_archived}</p>
-      <p>To: {activity[0].to}</p>
-      <p>Via: {activity[0].via}</p>
+      {/* I am ActivityDetail inside ActivityFeed */}
+      <div>
+        <span className="iconify" data-icon="flat-color-icons:missed-call" data-width="25" data-height="25"></span>
+      </div>
+      <div>
+        <p>From: {activity[0].from}</p>
+        <p>tried to call on {activity[0].via}</p>
+      </div>
+      <div>
+        <p>Created at: {activity[0].created_at}</p>
+      </div>
+      {/* <p>Number of calls: {activity.length}</p> */}
+      {/* <p>Call type: {activity[0].call_type}</p> */}
+      {/* <p>Direction: {activity[0].direction}</p> */}
+      {/* <p>Duration: {activity[0].duration}</p> */}
+      {/* <p>ID: {activity[0].id}</p> */}
+      {/* <p>Archived?: {activity[0].is_archived}</p> */}
+      {/* <p>To: {activity[0].to}</p> */}
+
     </div>
   )
 }
