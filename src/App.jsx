0c8d3995311290b9c4bf9187dd7fabfcf7288b8c
Dianne <mdianne.gabriel@gmail.com>
2021-10-18 22:06:18 -0400

    Install react-tabs

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 import ActivityFeed from './components/ActivityFeed.jsx';
 import Header from './Header.jsx';
+
+
 
 const App = () => {
   return (
     <div className='container'>
       <Header/>
       <div className="container-view">
-        <ActivityFeed />
+        <Tabs>
+          <TabList>
+            <Tab>Activity</Tab>
+            <Tab>Archived</Tab>
+          </TabList>
+
+          <TabPanel>
+            <ActivityFeed />
+          </TabPanel>
+          <TabPanel>
+            <h2>Any content 2</h2>
+          </TabPanel>
+        </Tabs>
       </div>
     </div>
   );
