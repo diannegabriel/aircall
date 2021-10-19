c9ff005edd2946ca4c05aa0f7888050123b21eb7
Dianne <mdianne.gabriel@gmail.com>
2021-10-19 12:09:01 -0400

    Import useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 import ActivityFeed from './components/ActivityFeed.jsx';
 import Header from './Header.jsx';
-
-
 
 const App = () => {
   return (
@@ -12,24 +10,6 @@ const App = () => {
       <Header/>
       <div className="container-view">
       <ActivityFeed />
-        {/* <Tabs>
-          <TabList>
-            <Tab>Recent Activities</Tab>
-            <Tab>Archived</Tab>
-            <Tab>All Calls</Tab>
-          </TabList>
-
-          <TabPanel>
-            <ActivityFeed />
-            <h2>Any content 1</h2>
-          </TabPanel>
-          <TabPanel>
-            <h2>Any content 2</h2>
-          </TabPanel>
-          <TabPanel>
-            <h2>Any content 3</h2>
-          </TabPanel>
-        </Tabs> */}
       </div>
     </div>
   );
--- src/components/ActivityFeed.jsx
+++ src/components/ActivityFeed.jsx
@@ -7,12 +7,7 @@ import '../css/ActivityFeed.css'
 
 const ActivityFeed = () => {
   return (
-    <Fragment>
-      {/* <div className="archive-calls">
-        <span className="iconify archive-icon" data-icon="fluent:archive-24-regular" data-width="25" data-height="25"></span>
-        <p>Archive all calls</p>
-      </div> */}
-      <Tabs>
+    <Tabs>
           <TabList>
             <Tab>Recent Activities</Tab>
             <Tab>Archived</Tab>
@@ -20,18 +15,22 @@ const ActivityFeed = () => {
           </TabList>
 
           <TabPanel>
-            {/* <ActivityFeed /> */}
-            <h2>Any content 1</h2>
+            <div className="archive-calls">
+              <span className="iconify archive-icon" data-icon="fluent:archive-24-regular" data-width="25" data-height="25"></span>
+              <p>Archive all calls</p>
+            </div>
             <ActivityDetail />
           </TabPanel>
           <TabPanel>
-            <h2>Any content 2</h2>
+            <div className="archive-calls">
+              <span className="iconify archive-icon" data-icon="fluent:archive-24-regular" data-width="25" data-height="25"></span>
+              <p>Unarchive all calls</p>
+            </div>
           </TabPanel>
           <TabPanel>
             <h2>Any content 3</h2>
           </TabPanel>
         </Tabs>
-    </Fragment>
   )
 }
 
