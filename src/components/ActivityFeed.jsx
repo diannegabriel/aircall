692810395e458c9457dd30635ff65d4fb7c3d74a
Dianne <mdianne.gabriel@gmail.com>
2021-10-18 21:58:38 -0400

    Build 'Archive all calls' model

--- src/components/ActivityFeed.jsx
+++ src/components/ActivityFeed.jsx
@@ -1,11 +1,15 @@
 import React, { Fragment } from 'react';
 
 import ActivityDetail from './ActivityDetail.jsx';
+import '../css/ActivityFeed.css'
 
 const ActivityFeed = () => {
   return (
     <Fragment>
-      <p>I am ActivityFeed</p>
+      <div className="archive-calls">
+        <span className="iconify archive-icon" data-icon="fluent:archive-24-regular" data-width="25" data-height="25"></span>
+        <p>Archive all calls</p>
+      </div>
       <ActivityDetail />
     </Fragment>
   )
