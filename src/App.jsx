147e91b8ce99a5d0f99597a5b06f8472a433b4fa
Dianne <mdianne.gabriel@gmail.com>
2021-10-19 12:28:09 -0400

    Implement handleStatusChange

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,12 @@ import ActivityFeed from './components/ActivityFeed.jsx';
 import Header from './Header.jsx';
 
 const App = () => {
+  const [status, setStatus] = useState('all');
   return (
     <div className='container'>
       <Header/>
       <div className="container-view">
-      <ActivityFeed />
+      <ActivityFeed setStatus={setStatus}/>
       </div>
     </div>
   );
--- src/components/ActivityFeed.jsx
+++ src/components/ActivityFeed.jsx
@@ -1,36 +1,42 @@
-import React, { Fragment } from 'react';
+import React, { useState } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+// import Tabs from 'react-bootstrap/Tabs'
+// import Tab from 'react-bootstrap/Tab'
 
 import ActivityDetail from './ActivityDetail.jsx';
 import 'react-tabs/style/react-tabs.css';
 import '../css/ActivityFeed.css'
 
-const ActivityFeed = () => {
+const ActivityFeed = ({ setStatus }) => {
+  const handleStatusChange = (e) => {
+    console.log(e)
+    setStatus(e)
+  }
   return (
-    <Tabs>
-          <TabList>
-            <Tab>Recent Activities</Tab>
-            <Tab>Archived</Tab>
-            <Tab>All Calls</Tab>
-          </TabList>
+    <Tabs onSelect={handleStatusChange}>
+      <TabList >
+        <Tab>All Calls</Tab>
+        <Tab>Missed</Tab>
+        <Tab>Archived</Tab>
+      </TabList>
 
-          <TabPanel>
-            <div className="archive-calls">
-              <span className="iconify archive-icon" data-icon="fluent:archive-24-regular" data-width="25" data-height="25"></span>
-              <p>Archive all calls</p>
-            </div>
-            <ActivityDetail />
-          </TabPanel>
-          <TabPanel>
-            <div className="archive-calls">
-              <span className="iconify archive-icon" data-icon="fluent:archive-24-regular" data-width="25" data-height="25"></span>
-              <p>Unarchive all calls</p>
-            </div>
-          </TabPanel>
-          <TabPanel>
-            <h2>Any content 3</h2>
-          </TabPanel>
-        </Tabs>
+      <TabPanel>
+        <h2>All Calls</h2>
+      </TabPanel>
+      <TabPanel>
+        <div className="archive-calls">
+          <span className="iconify archive-icon" data-icon="fluent:archive-24-regular" data-width="25" data-height="25"></span>
+          <p>Archive all calls</p>
+        </div>
+        <ActivityDetail />
+      </TabPanel>
+      <TabPanel>
+        <div className="archive-calls">
+          <span className="iconify archive-icon" data-icon="fluent:archive-24-regular" data-width="25" data-height="25"></span>
+          <p>Unarchive all calls</p>
+        </div>
+      </TabPanel>
+    </Tabs>
   )
 }
 
